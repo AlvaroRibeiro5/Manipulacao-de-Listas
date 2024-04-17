f33efdcac6ec6df9e531a5de57f1399b44f351f6
AlvaroRibeiro5 <128993949+AlvaroRibeiro5@users.noreply.github.com>
2024-04-17 18:21:08 -0300

    first commit

---
+++ TestRunner.java
@@ -0,0 +1,19 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**
+ * A classe TestRunner fornece suporte para a classe Testes funcionar devidamente.
+ * @version 1.0
+ */
+public class TestRunner {
+ public static void main() {
+   Result result = JUnitCore.runClasses(Testes.class);
+   
+   for (Failure failure : result.getFailures()) {
+     System.out.println(failure.toString());
+   }
+   
+   System.out.println(result.wasSuccessful());
+ }
+}
